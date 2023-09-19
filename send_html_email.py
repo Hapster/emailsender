2edbf982ae8157d7a8c608265fb1247af3373819
Hapster <saenenstef@hotmail.com>
2023-09-19 17:04:17 +0200

    Create send_html_email.py

---
+++ send_html_email.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.message import EmailMessage
+
+def send_hotmail_email(sender_email, sender_password, recipient_email, subject, html_message):
+    try:
+        # Create an EmailMessage object
+        email = EmailMessage()
+        email["From"] = sender_email
+        email["To"] = recipient_email
+        email["Subject"] = subject
+        email.set_content(html_message)
+
+        # Set the message content as HTML
+        email.add_alternative(html_message, subtype='html')
+      
+        # Connect to Hotmail"s SMTP server
+        smtp_server = "smtp-mail.outlook.com"
+        smtp_port = 587
+        smtp = smtplib.SMTP(smtp_server, smtp_port)
+        smtp.starttls()
+
+        # Login to your Hotmail account
+        smtp.login(sender_email, sender_password)
+
+        # Send the email
+        smtp.send_message(email)
+
+        # Close the SMTP server
+        smtp.quit()
+
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
